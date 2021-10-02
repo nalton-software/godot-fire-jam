354c2c8caab79af60b9b7405354107dceaae6361
Calbabreaker <calbabreaker@gmail.com>
2021-10-02 15:14:50 +0930

    added object toughness

--- scripts/Object.gd
+++ scripts/Object.gd
@@ -1,10 +1,31 @@
 extends StaticBody2D
 
 export var item_prefab: PackedScene
+export var spawn_offset := Vector2.ZERO
+export var toughness := 1.0
+
+onready var original_pos := position
+
+var mining := false
+var mining_progress := 0.0
+
+func _process(delta: float) -> void:
+	if mining:
+		mining_progress += delta
+		if mining_progress > toughness:
+			destroy()
+			
+		position = Utils.shake_once(original_pos,  mining_progress / toughness * 5.0)
+	
 
 func _on_Area2D_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if event is InputEventMouseButton && event.pressed:
-		var item = item_prefab.instance()
-		item.position = position
-		get_parent().add_child(item)
-		queue_free()
+	if event is InputEventMouseButton:
+		mining = event.pressed
+		mining_progress = 0.0
+		position = original_pos
+
+func destroy():
+	var item = item_prefab.instance()
+	item.position = position + spawn_offset
+	get_parent().add_child(item)
+	queue_free()
