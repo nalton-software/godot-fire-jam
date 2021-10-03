94a4bb485533c71681dfd5b16016fc9c4bcddfd2
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2021-10-03 18:14:50 +1030

    Add string{ {gacp}

--- scripts/items/Items.gd
+++ scripts/items/Items.gd
@@ -5,4 +5,5 @@ const Item = preload('res://scripts/items/Item.gd')
 var data = {
 	'Wood' : Item.new('Wood', preload('res://art/wood.png'), Vector2(48, 48)),
 	'Rock' : Item.new('Rock', preload('res://art/rock.png'), Vector2(48, 48)),
+	'String' : Item.new('String', preload('res://art/string.png'), Vector2(48, 48)),
 }
