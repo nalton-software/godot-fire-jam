4f393b70e4dd293f1c2e8436fea71bfc03ed95b4
Calbabreaker <calbabreaker@gmail.com>
2021-10-03 17:01:43 +1030

    added rock item

--- scripts/items/Items.gd
+++ scripts/items/Items.gd
@@ -3,5 +3,6 @@ extends Node
 const Item = preload('res://scripts/items/Item.gd')
 
 var data = {
-	'Wood' : Item.new('Wood', preload('res://art/wood.png'), Vector2(48, 48))
+	'Wood' : Item.new('Wood', preload('res://art/wood.png'), Vector2(48, 48)),
+	'Rock' : Item.new('Rock', preload('res://art/rock.png'), Vector2(48, 48)),
 }
