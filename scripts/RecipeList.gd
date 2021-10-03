48cb1e0f9f6a984a3dad6324b77f66c01d1804c5
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2021-10-03 14:54:52 +1030

    move items to scenes/ {gacp}

--- scripts/RecipeList.gd
+++ scripts/RecipeList.gd
@@ -3,5 +3,6 @@ extends Node
 const Recipe = preload('res://scripts/Recipe.gd')
 
 var data = [
-	Recipe.new('More wood', {'wood' : 2}, {'wood' : 3})
+	Recipe.new('Less wood', {'wood' : 2}, {'wood' : 1}),
+	Recipe.new('More wood', {'wood' : 2}, {'wood' : 3}),
 ]
