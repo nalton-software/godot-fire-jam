1620f3a53e4d0bab5880d541436ea508ebbc58f3
Calbabreaker <calbabreaker@gmail.com>
2021-10-03 15:24:17 +1030

    wip drop item

--- scripts/items/InventoryItem.gd
+++ scripts/items/InventoryItem.gd
@@ -1,5 +1,15 @@
 extends TextureRect
 
+var dragging_item := false
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and dragging_item:
+		$Item.rect_position = get_local_mouse_position()
+
 func set_item(item_data: ItemData):
 	$Item.texture = item_data.texture
 	remove_from_group("Empty")
+
+func _on_Item_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		dragging_item = event.pressed
