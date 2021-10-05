6f481c8c7620037233845dc6269f34c4252bb37b
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2021-10-05 15:23:33 +1030

    Make axe and string recipe {gacp}

--- scripts/items/Items.gd
+++ scripts/items/Items.gd
@@ -7,5 +7,6 @@ var data = {
 	'Stone' : Item.new('Stone', preload('res://art/stone.png'), Vector2(48, 48)),
 	'String' : Item.new('String', preload('res://art/string.png'), Vector2(48, 48)),
 	'Cotton Fluff' : Item.new('Cotton Fluff', preload('res://art/cotton_fluff.png'), Vector2(48, 48)),
-	'Cart' : Item.new('Cart', preload('res://art/cart.png'), Vector2(48, 48))
+	'Cart' : Item.new('Cart', preload('res://art/cart.png'), Vector2(48, 48)),
+	'Axe' : Item.new('Axe', preload('res://art/axe.png'), Vector2(48, 48))
 }
