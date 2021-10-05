f5741d92f813a400c5a2241d0d9a0dcce44d38b3
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2021-10-05 15:06:29 +1030

    Fix bug where crafted items would disappear {gacp}

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -3,8 +3,13 @@ extends CanvasLayer
 onready var inventory = $"InventoryItems"
 onready var item_slots = inventory.get_children()
 
-func add_item(item: Item, index: int):
-	item_slots[index].set_item(item)
+func add_item(item: Item, index: int = -1):
+	if index == -1:
+		for item_slot in item_slots:
+			if item_slot.is_empty():
+				item_slot.set_item(item)
+	else:
+		item_slots[index].set_item(item)
 
 func remove_item(index: int):
 	var spots = inventory.get_children()
